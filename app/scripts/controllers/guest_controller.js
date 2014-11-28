11d55041ac2c64c3a31c58a15a12ecd4f4643e30
Matt Griffeth <mgriffeth9@gmail.com>
2014-11-28 13:47:47 -0500

    okok

--- app/scripts/controllers/guest_controller.js
+++ app/scripts/controllers/guest_controller.js
@@ -1,7 +1,11 @@
 (function(){
   angular.module('CookForMe')
-    .controller('GuestController',['$scope', function(scope){
+    .controller('GuestController',['$scope','GuestFactory', function($scope, GuestFactory){
 
+
+        scope.newUser = function(user){
+          GuestFactory.newUser(user);
+        };
 
 
 

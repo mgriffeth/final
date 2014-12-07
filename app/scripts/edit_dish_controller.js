e555fb208a773922662ae3415e3fe7b8cf28f6f4
Matt Griffeth <mgriffeth9@gmail.com>
2014-12-06 20:40:20 -0500

    need style

--- app/scripts/edit_dish_controller.js
+++ app/scripts/edit_dish_controller.js
@@ -3,11 +3,19 @@
     .controller('EditDishController',['$scope','$location','$http','Parse_Headers','AllUsersFactory','$cookieStore','$cookies', '$routeParams',
     function($scope,$location, $http, Parse_Headers, AllUsersFactory, $cookieStore, $cookies, $routeParams){
 
+      var user = $cookieStore.get('activeUser');
+      console.log(user);
       var baseUrl = 'https://api.parse.com/1/';
       $http.get(baseUrl + 'classes/dishClass/' + $routeParams.did, Parse_Headers).success(function(data){
         console.log(data);
         $scope.dish = data;
-      })
+      });
+
+      $scope.edit = function(dish){
+        $http.put(baseUrl + "classes/dishClass/" + $routeParams.did, dish ,Parse_Headers).success(function(){
+          $location.path('/cookProfile')
+        });
+      }
 
     }
   ]);
